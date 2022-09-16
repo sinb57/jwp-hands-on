0a8dfba1adcfc9d2e0b5bc2c5a08036f347376aa
sinb57 <sinb57@naver.com>
2022-09-16 11:36:59 +0900

    test: Junit3TestRunner 통과

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,10 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +14,19 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test junit3Test = new Junit3Test();
+
+        String targetMethodPrefix = "test";
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> isNameStartWith(method.getName(), targetMethodPrefix))
+                .collect(Collectors.toUnmodifiableList());
+
+        for (Method method : methods) {
+                method.invoke(junit3Test);
+        }
+    }
+
+    private boolean isNameStartWith(String methodName, String prefix) {
+        return methodName.startsWith(prefix);
     }
 }
